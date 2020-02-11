51e0eb5d3c285a9df790e1f77410d8865f53e0b8
christopher watts <chris@byhook.com>
2020-02-11 11:34:32 -0800

    mouse events for CTA

--- js/Animation.js
+++ js/Animation.js
@@ -2,6 +2,18 @@
     windowRef.BannerAnimation =
     function () {
 
+
+        document.querySelectorAll('.catch-all').forEach(function (elem){
+
+            elem.addEventListener('mouseover',function (e){
+                console.log ('Over Handler')
+            });
+            elem.addEventListener('mouseout',function (e){
+                console.log ('out Handler')
+            });
+        });
+
+
         console.log ("run animation");
         let tl = window.gsap.timeline();
         tl.to("#adRoot", {duration: .2,opacity: 0.5});
